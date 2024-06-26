1bc3ae6ebceb7913ab39c9cdbc2e0aef17eae60f
Diaconu Radu-Mihai <52667211+radudiaconu0@users.noreply.github.com>
2024-06-26 17:52:58 +0300

    fix category -> product relation

--- database/migrations/2024_06_26_141214_create_products_table.php
+++ database/migrations/2024_06_26_141214_create_products_table.php
@@ -9,7 +9,7 @@ return new class extends Migration {
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
-            $table->uuid('category_uuid');
+            $table->uuid('category_id');
             $table->uuid('uuid');
             $table->string('title');
             $table->float('price');
@@ -18,7 +18,7 @@ return new class extends Migration {
             $table->softDeletes();
             $table->timestamps();
 
-            $table->foreign('category_uuid')->references('uuid')->on('categories');
+            $table->foreign('category_id')->references('uuid')->on('categories');
         });
     }
 
