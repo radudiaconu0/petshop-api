399fc78a52653927c117e1d6f31e08040950d77d
Diaconu Radu-Mihai <52667211+radudiaconu0@users.noreply.github.com>
2024-07-02 21:15:48 +0300

    - add statsu to response helper

--- app/Helpers/ResponseHelper.php
+++ app/Helpers/ResponseHelper.php
@@ -4,7 +4,7 @@ namespace App\Helpers;
 
 class ResponseHelper
 {
-    public static function success($data = [], $extra = [])
+    public static function success($data = [], $extra = [], $status = 200)
     {
         return response()->json([
             'success' => 1,
@@ -12,7 +12,7 @@ class ResponseHelper
             'error' => null,
             'errors' => [],
             'extra' => $extra,
-        ]);
+        ], $status);
     }
 
     public static function error($message = 'An error occurred', $errors = [], $extra = [], $status = 400)
@@ -23,6 +23,6 @@ class ResponseHelper
             'error' => $message,
             'errors' => $errors,
             'extra' => $extra,
-        ]);
+        ], $status);
     }
 }
